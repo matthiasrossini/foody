90f8bf5bba854373930d41893a9a460ab8b33d42
Adrien Walewski <adrien.walewski@gmail.com>
2020-11-23 10:56:29 +0100

    Just imported a PasswordField to check if commits work!

--- foody/forms.py
+++ foody/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FloatField, IntegerField, TextAreaField
+from wtforms import StringField, SubmitField, FloatField, IntegerField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired, Length
+
 
 class TableForm(FlaskForm):
     number_guests = IntegerField("Number of people:", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+
 class MenuForm(FlaskForm):
-	name = StringField('name', validators=[DataRequired()])
-	description = TextAreaField('Content', validators=[DataRequired(), Length(min=1, max=200)])
-	submit = SubmitField('Save')
+    name = StringField('name', validators=[DataRequired()])
+    description = TextAreaField('Content', validators=[DataRequired(), Length(min=1, max=200)])
+    submit = SubmitField('Save')
