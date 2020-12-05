8890a9a08dbb8d3692cf34cbfe6c92e46fc043a3
Damian zaker <>
2020-12-05 12:45:33 +0100

    new branch

--- foody/routes.py
+++ foody/routes.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 
 from foody import app, db  # data
 from foody.forms import TableForm, ProductUpload, AddAdmin, AdminLogin, MenuForm, AddWaiter, WaiterLogin, SubmitOrder
-from foody.models import Products, Table, Orders, get_products, get_orders, User, register_login, load_user, add_admin, check_admin, add_waiter, check_waiter, logout_client
+from foody.models import Products, Orders, get_products, get_orders, User, register_login, load_user, add_admin, check_admin, add_waiter, check_waiter, logout_client
 
 from flask_login import LoginManager, UserMixin, login_user, current_user
 from flask_login import logout_user, login_required
--- foody/test.py
+++ foody/test.py
@@ -1,2 +1,3 @@
+from models import Products, Table, Orders, get_products, get_orders, User, register_login, load_user, add_admin, check_admin, add_waiter, check_waiter, logout_client
 
 my_order = Orders.query.first()
